a624f863ce2f354097fe30ba14a1cb5b408639d9
yeongo <yeongo@student.42seoul.kr>
2023-07-17 16:09:57 +0900

    finish cpp-module 01

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,43 +1,58 @@
+#include <cstring>
 #include <fstream>
-#include <sstream>
-#include <sys/_types/_size_t.h>
+#include <iostream>
 
 int	main(int argc, char *argv[]) {
-	if (argc != 4)
+	if (argc != 4) {
+		std::cout << "Only 4 arguments are allowed" << std::endl;
 		return (1);
-	std::string	filename = argv[1];
-	std::string	s1 = argv[2];
-	std::string	s2 = argv[3];
-	std::string line;
+	}
 
+	std::string		filename = argv[1];
+	std::string		s1 = argv[2];
+	std::string		s2 = argv[3];
+	const size_t	s1_length = s1.length();
+	const size_t	s2_length = s2.length();
+
+	std::string		contents;
 	std::ifstream	infile;
-	infile.open(argv[1]);
-	std::string	data;
-	infile >> data;
-
 	std::ofstream	outfile;
-	outfile.open(filename + ".replace");
 
-	while (true)
-	{
-		std::getline(infile, line);
+	if (std::strlen(argv[1]) == 0 || s1_length == 0 || s2_length == 0) {
+		std::cout << "Argument is empty" << std::endl;
+		return (1);
+	}
+
+	infile.open(filename);
+	if (infile.fail()) {
+		std::cout << "Can't open infile" << std::endl;
+		return (1);
+	}
+
+	outfile.open(filename + ".replace");
+	if (outfile.fail()) {
+		std::cout << "Can't open outfile" << std::endl;
+		return (1);
+	}
+
+	while (true) {
+		std::getline(infile, contents);
 		
 		size_t pos = 0;
-		while (true)
-		{
-			pos = line.find(s1, pos);
+		while (true) {
+			pos = contents.find(s1, pos);
 			if (pos == std::string::npos)
-			{
 				break ;
-			}
-			line.erase(pos, s1.length());
-			line.insert(pos, s2);
-			pos += s2.length();
+			contents.erase(pos, s1_length);
+			contents.insert(pos, s2);
+			pos += s2_length;
 		}
-		outfile << line;
+		outfile << contents;
 		if (infile.eof())
 			break ;
 		outfile << std::endl;	
 	}
+	infile.close();
+	outfile.close();
 	return (0);
 }
---
+++ ex06/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+#include <iostream>
+#include <string>
+int	main(int argc, char *argv[]) {
+	if (argc != 2)
+	{
+		std::cout << "Only 2 arguments are allowed" << std::endl;
+		return (1);
+	}
+
+	Harl	harl;
+	std::string	level = argv[1];
+	harl.complain(level);
+	return (0);
+}
